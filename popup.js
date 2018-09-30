9f7485b5eded63fcee8ea2475299321b34e0d0dd
phaninder pasupula <pasupulaphani@gmail.com>
2018-09-30 18:29:41 +0100

    show times

--- popup.js
+++ popup.js
@@ -1,44 +1,61 @@
-// row content update
-function updateRowContent(rownum) {
-  // return Promise.all([
-  //   getStore('tickers'),
-  //   getStore('tokens')
-  // ])
-  // .then((res) => {
-  //   const tickers = res[0]
-  //   const tokens = res[1]
-  //   const contractAddresses = tickers.map(ticker => {
-  //     return tokens
-  //       .find(t => t.ticker.toLowerCase() === ticker.toLowerCase())
-  //       .contractAddress
-  //   })
-  // })
-
+function updateTable() {
   const table = document.getElementById('mainTableBody')
 
-  var ticker = table.rows[rownum].cells[0].innerHTML;
-  console.log("[updateRowContent]: ", ticker)
+  return Promise.all([
+    getStore('tickers'),
+    getStore('tokens'),
+    getStore('holdings-24'),
+    getStore('holdings-120')
+  ])
+  .then((res) => {
+    const tickers = res[0]
+    const tokens = res[1]
+    const holdings24 = res[2]
+    const holdings120 = res[3]
 
-  var volumeDelta3 = 5
-  table.rows[rownum].cells[1].innerHTML = volumeDelta3;
+    tickers.forEach((ticker, rownum) => {
+      console.log("[updateTable]: ", ticker)
 
-  var volumeDelta24 = 8
-  table.rows[rownum].cells[2].innerHTML = volumeDelta24;
+      const row = table.insertRow(rownum);  
+      [ticker.toUpperCase(), 'N/A', 'N/A', 'N/A', 'N/A'].forEach(function (element) {
+        let newcell = row.insertCell(-1);
+        newcell.innerHTML = element;
+      })
+      const contractAddress = tokens
+        .find(t => t.ticker.toLowerCase() === ticker.toLowerCase())
+        .contractAddress
 
-  var newPrice = 2
-  var oldPrice = table.rows[rownum].cells[3].innerHTML = volumeDelta24;
-  table.rows[rownum].cells[3].innerHTML = newPrice;
-  if (newPrice > oldPrice) {
-    table.rows[rownum].cells[3].style.color = "green";
-  } else {
-    table.rows[rownum].cells[3].style.color = "red";
-  }
+      var volumeDelta3 = getHoldingChange(holdings120[contractAddress])
+      table.rows[rownum].cells[1].innerHTML = volumeDelta3;
 
-  var removeBtn = '<a class="delete" title="Delete" data-toggle="tooltip" data-original-title="Delete"><i class="material-icons">delete</i></a>'
-  table.rows[rownum].cells[4].innerHTML = removeBtn;
-  table.rows[rownum].cells[4].id = ticker
-  table.rows[rownum].cells[4].addEventListener("click", deleteTicker);
+      var volumeDelta24 = getHoldingChange(holdings24[contractAddress])
+      table.rows[rownum].cells[2].innerHTML = volumeDelta24;
 
+      var newPrice = 2
+      var oldPrice = table.rows[rownum].cells[3].innerHTML = volumeDelta24;
+      table.rows[rownum].cells[3].innerHTML = newPrice;
+      if (newPrice > oldPrice) {
+        table.rows[rownum].cells[3].style.color = "green";
+      } else {
+        table.rows[rownum].cells[3].style.color = "red";
+      }
+
+      const removeBtn = '<a class="delete" title="Delete" data-toggle="tooltip" data-original-title="Delete"><i class="material-icons">delete</i></a>'
+      table.rows[rownum].cells[4].innerHTML = removeBtn;
+      table.rows[rownum].cells[4].id = ticker
+      table.rows[rownum].cells[4].addEventListener("click", deleteTicker);
+
+    })
+  })
+}
+
+function getHoldingChange(holdings) {
+  const delta = (holdings.maxTimestampShares - holdings.minTimestampShare) / holdings.maxTimestampShares
+  return (delta * 100).toFixed(1)
+}
+
+function clearTable() {
+  $('#mainTableBody').empty();
 }
 
 function deleteTicker() {
@@ -52,49 +69,9 @@ function deleteTicker() {
         setStore('tickers', tickers)
           .then(() => {
             clearTable()
-            loadTable()
+            updateTable()
           });
       }
-    })
-}
-
-// table content update
-function updateTableContent(tableBodyId) {
-  var i = 0;
-  $(`#${tableBodyId} tr`).each(function() {
-    // for each row call updateRowContent
-    console.log("Iterating through table")
-    updateRowContent(i)
-    i += 1
-  });
-}
-
-// Row creation
-function appendRow(newrow) {
-  var table = document.getElementById('mainTableBody');
-  var row = table.insertRow(0);
-  newrow.forEach(function(element){
-    let newcell = row.insertCell(-1);
-    newcell.innerHTML = element;
-  })
-}
-
-function clearTable() {
-  $('#mainTableBody').empty();
-}
-
-function loadTable() {
-  return getStore('tickers')
-    .then((tickers) => {
-      // console.log("The saved tickers:" + tickers)
-      tickers.forEach(function(ticker){
-        // construct row
-        const newrow = [ticker.toUpperCase(), 1, 2, 3, 4]
-        // append row
-        appendRow(newrow)
-        // update this row
-        updateRowContent(0)
-      })
     })
 }
 
@@ -163,10 +140,7 @@ $("#token_name_input").easyAutocomplete({
             tickers.push(newTicker)
             return setStore('tickers', tickers)
               .then((data) => {
-                console.log("The new array was saved:" + data);
-                appendRow([newTicker, 1, 2, 3, 4])
-                // update/populate this row
-                updateRowContent(0)
+                return updateTable()
               });
           }
         })
@@ -198,8 +172,7 @@ function clearAlarm() {
 $(document).ready(function () {
   loadTokensJson()
   loadHoldingsData()
-  loadTable()
-  updateTableContent("mainTableBody")
+  updateTable()
 });
 
 
@@ -209,3 +182,4 @@ $(document).ready(function () {
 // tokens : total tokens available
 // holdings-24 : 24 hour holdings for the tickers
 // holdings-120 : 120 hour holdings for the tickers
+// table-data : display table info
