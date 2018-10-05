15ef3790981185d1c642ecb5835fd35889e75250
phaninder pasupula <pasupulaphani@gmail.com>
2018-10-05 13:08:36 +0100

    more steps

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-127056435-1']);
+_gaq.push(['_trackPageview']);
+
+(function () {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 chrome.runtime.onInstalled.addListener(function() {
   // Default tickers selection
   chrome.storage.sync.set({tickers: ["mkr", "zrx"]}, function() {
